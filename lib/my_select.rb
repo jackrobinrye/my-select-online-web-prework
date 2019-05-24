558e3b94964533cc4b5aa5feea2a583c5356df07
Jack Atherton <jackrrye@gmail.com>
2019-05-24 00:17:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ collection2 = []
+ i=0 
+ while i < collection.size do
+   if yield(collection[i]) == true 
+     collection2 << collection[i]
+   else 
+     collection2
+   end 
+ end
+collection2 
 end
